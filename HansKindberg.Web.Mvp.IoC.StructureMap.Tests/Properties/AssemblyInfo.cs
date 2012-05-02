3d079369a2be982ea7554ed73fe4dac87f8211d3
Hans Kindberg <hans.kindberg@gmail.com>
2012-05-02 15:02:13 +0200

    ReSharper clean up.

--- HansKindberg.Web.Mvp.IoC.StructureMap.Tests/Properties/AssemblyInfo.cs
+++ HansKindberg.Web.Mvp.IoC.StructureMap.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("Tests for HansKindberg.Web.Mvp.IoC.StructureMap")]
