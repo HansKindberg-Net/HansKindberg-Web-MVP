4e658a0e1b7944800aadf091ca1ef5f01324fcb5
Hans Kindberg <hans.kindberg@gmail.com>
2012-10-05 13:22:30 +0200

    Added inconclusive tests to remember types that needs tests.

--- HansKindberg.Web.Mvp.Tests/UI/Views/ControlViewTest.cs
+++ HansKindberg.Web.Mvp.Tests/UI/Views/ControlViewTest.cs
@@ -1,7 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using HansKindberg.Web.Mvp.UI.Views;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HansKindberg.Web.Mvp.Tests.UI.Views
 {
 	[TestClass]
-	public class ControlViewTest {}
+	public class ControlViewTest
+	{
+		#region Methods
+
+		[TestMethod]
+		public void RemoveThisWhenYouWriteRealTests_ItIsJustHereSoWeKeepAReferenceToTheTypeWeNeedToTest_IfTheTypeThatThisTestIsForWillBeRemovedWeWantForgetToRemoveThisTestClassEitherBecauseItWillNotCompile()
+		{
+			Assert.Inconclusive(typeof(ControlView).FullName + " need more tests.");
+		}
+
+		#endregion
+	}
 }
--- HansKindberg.Web.Mvp.Tests/UI/Views/TemplatedControlViewTest.cs
+++ HansKindberg.Web.Mvp.Tests/UI/Views/TemplatedControlViewTest.cs
@@ -1,7 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using HansKindberg.Web.Mvp.UI.Views;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HansKindberg.Web.Mvp.Tests.UI.Views
 {
 	[TestClass]
-	public class TemplatedControlViewTest {}
+	public class TemplatedControlViewTest
+	{
+		#region Methods
+
+		[TestMethod]
+		public void RemoveThisWhenYouWriteRealTests_ItIsJustHereSoWeKeepAReferenceToTheTypeWeNeedToTest_IfTheTypeThatThisTestIsForWillBeRemovedWeWantForgetToRemoveThisTestClassEitherBecauseItWillNotCompile()
+		{
+			Assert.Inconclusive(typeof(TemplatedControlView).FullName + " need more tests.");
+		}
+
+		#endregion
+	}
 }
