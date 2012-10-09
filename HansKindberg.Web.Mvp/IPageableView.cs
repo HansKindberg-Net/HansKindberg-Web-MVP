ebcc326942b7aa49b8c149ad4472c83d5073b2fd
Hans Kindberg <hans.kindberg@gmail.com>
2012-10-09 08:29:43 +0200

    Added IPageableView.

---
+++ HansKindberg.Web.Mvp/IPageableView.cs
@@ -0,0 +1,14 @@
+﻿namespace HansKindberg.Web.Mvp
+{
+	public interface IPageableView
+	{
+		#region Properties
+
+		int ItemsPerPagingItem { get; set; }
+		bool PagingEnabled { get; set; }
+		int PagingItems { get; }
+		int PagingPosition { get; }
+
+		#endregion
+	}
+}
