a84252e62ef765b59cbef8066a4b1eaf82e27f86
Hans Kindberg <hans.kindberg@gmail.com>
2012-10-18 23:42:13 +0200

    New types

---
+++ HansKindberg.Web.Mvp/UI/Views/AutoDataBindableUserControlView.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace HansKindberg.Web.Mvp.UI.Views
+{
+	public abstract class AutoDataBindableUserControlView : UserControlView, IAutoDataBindableView
+	{
+		#region Fields
+
+		private const string _autoDataBindViewStateName = "AutoDataBind";
+
+		#endregion
+
+		#region Events
+
+		public event EventHandler CreatingChildControls;
+		public event EventHandler<CancelEventArgs> DataBindingChildren;
+		public event EventHandler<CancelEventArgs> EnsuringChildControls;
+
+		#endregion
+
+		#region Properties
+
+		[Category("Information"), DefaultValue(false)]
+		public virtual bool AutoDataBind
+		{
+			get { return (this.ViewState[_autoDataBindViewStateName] as bool? ?? (bool?) false).Value; }
+			set { this.ViewState[_autoDataBindViewStateName] = value; }
+		}
+
+		[Browsable(false)]
+		public new virtual bool DesignMode
+		{
+			get { return base.DesignMode; }
+		}
+
+		#endregion
+
+		#region Methods
+
+		protected override void CreateChildControls()
+		{
+			if(this.CreatingChildControls != null)
+				this.CreatingChildControls(this, EventArgs.Empty);
+		}
+
+		public override void DataBind()
+		{
+			this.DataBind(true, true, true);
+		}
+
+		public virtual void DataBind(bool raiseOnDataBinding, bool ensureChildControls, bool dataBindChildren)
+		{
+			if(raiseOnDataBinding)
+				this.OnDataBinding(EventArgs.Empty);
+
+			if(ensureChildControls)
+				this.EnsureChildControls();
+
+			if(dataBindChildren)
+				this.DataBindChildren();
+		}
+
+		protected override void DataBindChildren()
+		{
+			if(this.DataBindingChildren != null)
+			{
+				CancelEventArgs cancelEventArgs = new CancelEventArgs();
+
+				this.DataBindingChildren(this, cancelEventArgs);
+
+				if(cancelEventArgs.Cancel)
+					return;
+			}
+
+			base.DataBindChildren();
+		}
+
+		protected override void EnsureChildControls()
+		{
+			if(this.EnsuringChildControls != null)
+			{
+				CancelEventArgs cancelEventArgs = new CancelEventArgs();
+
+				this.EnsuringChildControls(this, cancelEventArgs);
+
+				if(cancelEventArgs.Cancel)
+					return;
+			}
+
+			base.EnsureChildControls();
+		}
+
+		#endregion
+	}
+
+	public abstract class AutoDataBindableUserControlView<TModel> : AutoDataBindableUserControlView, IAutoDataBindableView<TModel> where TModel : class, new()
+	{
+		#region Fields
+
+		private TModel _model;
+
+		#endregion
+
+		#region Properties
+
+		public virtual TModel Model
+		{
+			get
+			{
+				ViewHelper.ThrowExceptionIfModelIsNull(this._model);
+
+				return this._model;
+			}
+			set { this._model = value; }
+		}
+
+		#endregion
+	}
+}
---
+++ HansKindberg.Web.Mvp/UI/Views/MasterPageView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI;
+using WebFormsMvp.Web;
+
+namespace HansKindberg.Web.Mvp.UI.Views
+{
+	public abstract class MasterPageView : MasterPage, WebFormsMvp.IView
+	{
+		#region Properties
+
+		[Browsable(false)]
+		public virtual bool ThrowExceptionIfNoPresenterBound
+		{
+			get { return true; }
+		}
+
+		#endregion
+
+		#region Eventhandlers
+
+		protected override void OnInit(EventArgs e)
+		{
+			PageViewHost.Register(this, this.Context, false);
+			base.OnInit(e);
+		}
+
+		#endregion
+	}
+
+	public abstract class MasterPageView<TModel> : MasterPageView, WebFormsMvp.IView<TModel> where TModel : class, new()
+	{
+		#region Fields
+
+		private TModel _model;
+
+		#endregion
+
+		#region Properties
+
+		public virtual TModel Model
+		{
+			get
+			{
+				ViewHelper.ThrowExceptionIfModelIsNull(this._model);
+
+				return this._model;
+			}
+			set { this._model = value; }
+		}
+
+		#endregion
+	}
+}
